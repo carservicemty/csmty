5d9e586aa8ca3403a52d24f142332bd7249ca5e7
carservicemty <141686584+carservicemty@users.noreply.github.com>
2026-03-28 13:51:16 -0600

    Update index.js

--- src/pages/index.js
+++ src/pages/index.js
@@ -7,12 +7,12 @@ import { RawHtml, Override, StackItem, Stack } from "@quarkly/components";
 import { MdArrowDownward } from "react-icons/md";
 export default (() => {
 	return <Theme theme={theme}>
-		<GlobalQuarklyPageStyles pageUrl={"index"} />
 		<Helmet titleTemplate="%s">
 			<title>Car Service MTY</title>
 			<meta name={"description"} content={"Taller mecánico en Monterrey"} />
 			<link rel={"shortcut icon"} href={"https://uploads.quarkly.io/readme/cra/favicon-32x32.ico"} type={"image/x-icon"} />
 		</Helmet>
+		<GlobalQuarklyPageStyles pageUrl={"index"} />
 		<Box
 			min-width="100px"
 			min-height="100px"
